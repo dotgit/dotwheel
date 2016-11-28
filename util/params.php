2418e1fac1e89d5c317a3dd3bd9dd6056ef9df04
Stas Trefilov <stas.trefilov@gmail.com>
2016-11-28 01:06:48 +0100

    - Misc\Params covered

--- util/params.php
+++ util/params.php
@@ -12,20 +12,22 @@ namespace Dotwheel\Util;
 
 class Params
 {
-    /** inject a new attributes into the list of attributes or add new value to the existing attribute
+    /** add new attribute to the list of attributes or add new value to the existing attribute
      * @param array $params array of attributes (reference)
      * @param string $value attribute value
      * @param string $name  attribute name
      * @param string $sep   separator of attribute values
+     * @assert() == 1
      */
     public static function add(&$params, $value, $name='class', $sep=' ')
     {
-        if (! \is_array($params))
+        if (! \is_array($params)) {
             $params = array($name=>$value);
-        elseif (empty($params[$name]))
+        } elseif (empty($params[$name])) {
             $params[$name] = $value;
-        elseif (\strpos("$sep{$params[$name]}$sep", "$sep$value$sep") === false)
+        } elseif (\strpos("$sep{$params[$name]}$sep", "$sep$value$sep") === false) {
             $params[$name] .= "$sep$value";
+        }
     }
 
     /** returns the value of the specified attribute and unsets it in the attributes array
@@ -33,11 +35,13 @@ class Params
      * @param string $name      attribute name
      * @param mixed $default    default value to return if $params[$name] is not set
      * @return mixed
+     * @assert() == 1
      */
     public static function extract(&$params, $name, $default=null)
     {
-        if (! \is_array($params) or ! \array_key_exists($name, $params))
+        if (! \is_array($params) or ! \array_key_exists($name, $params)) {
             return $default;
+        }
 
         $ret = $params[$name];
         unset($params[$name]);
