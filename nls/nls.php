676c6d8c3a1b930553fbeb5eebdc1f612e81de6d
Stas Trefilov <stas.trefilov@gmail.com>
2023-07-19 02:13:23 +0200

    - fix: correct format for datepicker in english locale

--- nls/nls.php
+++ nls/nls.php
@@ -97,7 +97,7 @@ class Nls
             ],
             self::P_WDAYS_SHORT => ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
             self::P_GMAPS_FMT => 'http://maps.google.com/?q=%s',
-            self::P_DATEPICKER => ['language' => 'en'],
+            self::P_DATEPICKER => ['language' => 'en', 'format' => 'dd/mm/yy'],
         ],
         'fr' => [
             self::P_NAME => 'Français',
